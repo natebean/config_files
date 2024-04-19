eb507e783ef759a30efa1f2e82b63eb2bfefaacc
Nathan Bean <natebean@pop-os.localdomain>
2024-04-18 20:13:24 -0500

    tmux update and clean up

--- nvim/lua/user/telescope.lua
+++
@@ -1,7 +0,0 @@
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files"})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep"})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List buffers"})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "List help tags"})
-vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "List keymaps"})
